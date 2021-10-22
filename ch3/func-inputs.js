a30912bfbd6a998b6e4468443becbc9f2034228d
rsgilbert <gssenyonjo@gmail.com>
2021-10-22 04:19:43 +0000

    loose curry

--- ch3/func-inputs.js
+++ ch3/func-inputs.js
@@ -1,3 +1,7 @@
+// A home made library with helper functions for manipulating
+// how we deal with function input.
+// See: https://github.com/getify/Functional-Light-JS/blob/master/manuscript/ch3.md
+
 // All for one
 // Helper function that ensures only one 
 // argument is passed through to function fn .
@@ -61,7 +65,8 @@ function constantUsage() {
 
 // Adapting arguments to parameters
 // 1. A function that spreads out a single received array as 
-// its individual arguments
+// its individual arguments and returns a function that calls
+// the given function with the arguments to spread out supplied as an array.
 // This function is sometimes called apply
 // spreadArgs takes in a function that expects arguments already spread.
 // It returns a function that accepts an array of arguments.
@@ -103,6 +108,211 @@ function gatherArgsUsage() {
     // gatherArgs() and passed to f1 as a single array
     f2(gatherArgs(f1))
 }
-gatherArgsUsage()
+// gatherArgsUsage()
 
 // Some Now, Some Later
+
+// Partial application
+// Partial application is a reduction in a 
+// function's arity. Arity is the number of expected
+// parameter inputs.
+// Partial function works like this: I have this function fn
+// and I have to pass in these many arguments, but I would like to
+// pass in only a few right now, so allow me to give you the few I have,
+// Pass them in for me and return me a function that I can use to pass in
+// the remaining arguments later.
+function partial(fn, ...presetArgs) {
+    // Inner function partiallyAppliedFn(..) closes over
+    // partial's parameters ie. fn and presetArgs so it
+    // can keep accessing them later no matter where the function
+    // runs.
+    return function partiallyAppliedFn(...laterArgs) {
+        return fn(...presetArgs, ...laterArgs)
+    }
+}
+
+function partialUsage(){ 
+    function f1(a, b, c, d, e) { console.log(a + b + c + d + e); };
+    const f2 = partial(f1, 1)
+    const f3 = partial(f2, 2)
+    const f4 = partial(f3, 3, 4)
+    f4(5)
+
+    // Add 5
+    function add(x, y) { return x + y }
+    let arr2 = [1, 2, 3, 4].map(partial(add, 5))
+    console.log(arr2) // 6, 7, 8, 9
+}
+
+// partialUsage()
+
+// Reverse arguments
+// Function that wraps a function to reverse its argument order
+// Produces a function that expects arguments to be given to it
+// in reverse order
+function reverseArgs(fn) {
+    return function argsReversedFn(...args) {
+        return fn(...args.reverse())
+    }
+}
+
+function reverseArgsUsage(){
+    // Reverse order of arguments
+    function diff(a, b, c, d) { return a - b - c - d; }
+    console.log(diff(1, 2, 3, 4))
+    const diffReversedArgs = reverseArgs(diff)
+    console.log(diffReversedArgs(4, 3, 2, 1))
+    const difRevPart = partial(diffReversedArgs, 4, 3)
+    console.log(difRevPart(2, 1))   
+}
+// reverseArgsUsage()
+
+// Partially apply right most arguments.
+// The remaining arguments should also be specified right to left
+function partialRight1(fn, ...presetArgs) {
+    // We provide the first batch of arguments to apply
+    // Outer reverseArgs is to reverse arguments order for laterArgs
+    return reverseArgs(
+        partial(reverseArgs(fn), ...presetArgs.reverse())
+    )
+}
+
+// Partially apply right most arguments. Alternative implementation.
+// Note that right most does not mean arguments for last parameters.
+// The right most means right most relative to other passed in arguments. 
+function partialRight(fn, ...presetArgs) {
+    return function partiallyAppliedRight(...laterArgs) {
+        return fn(...laterArgs, ...presetArgs)
+    }
+}
+
+function partialRightUsage() {
+    function f1(a, b, c, d) { return ((a - b) * c) + d  }
+    const p1 = partialRight(f1, 2, 3)
+    console.log(p1(1, 0)) // expect: ((1 - 0) * 2) + 3) = 5
+
+    // There is no guarantee that the last parameters will have values
+    // passed to them 
+    // Miss out some
+    function f2(w, x, y, z, ...rest) {
+        console.log(w, x, y, z, rest)
+    }
+    let f = partialRight(f2, 'z:last')
+    f(1, 2) // 1 2 'z:last' undefined []
+    f(1)
+    f(1,2,3)
+    f(1,2,3,4)
+    f(1,2,3,4,5)
+}
+// partialRightUsage()
+
+// One at a time
+// Currying. This is a technique where a function that
+// expects multiple arguments is broken down into 
+// successive chained functions that each take a single 
+// argument (arity: 1) and return another function to 
+// accept the next argument.
+// Currying unwinds a single higher-arity function into
+// a series of chained unary functions.
+function curry(fn, arity = fn.length) {
+    return (function nextCurried(prevArgs) {
+        return function curried(nextArg) {
+            let args = [...prevArgs, nextArg]
+            if(args.length >= arity) {
+                return spreadArgs(fn)(args)
+            }
+            else {
+                return nextCurried(args)
+            }
+        }
+    })([])
+}
+  
+const curryArrow = 
+    (fn, arity = fn.length, nextCurried) =>
+        (nextCurried = prevArgs =>
+            nextArg => {
+                // Add received arg to args collection
+                let args = [...prevArgs, nextArg]
+                if(args.length >= arity) {
+                    // We have nough args, execute
+                    return fn(...args)
+                } 
+                else {
+                    // Return another curried function to
+                    // collect the next arg
+                    return nextCurried(args)
+                }
+            }
+        )([])
+
+        
+function curryUsage() {
+    function f1(a, b, c) { console.log(a + b + c) }
+    const curriedF1 = curry(f1)
+    const c1 = curriedF1(1)
+    console.log(typeof c1)
+    const c2 = c1(2)
+    console.log(typeof c2)
+    const c3 = c2(3)
+    console.log(typeof c3) // undefined 
+    curryArrow(f1)(3)(1)(5) // 9
+
+    // Add
+    const add = (a, b) => a + b
+    let arr = [1, 2, 3, 4].map(curry(add)(2)) 
+    console.log(arr) // [3, 4, 5, 6]
+
+    // Sum
+    const sum = (...args) => {
+        let total = 0;
+        for(let num of args) 
+            total += num;
+        return total;
+    }
+    console.log(curry(sum, 4)(1)(2)(3)(4)) // 10
+
+}
+// curryUsage()
+
+// Currying multiple arguments at once
+function looseCurry(fn, arity = fn.length) {
+    return (function nextCurried(prevArgs) {
+        return function curried(...nextArgs) {
+            const args = [...prevArgs, ...nextArgs]
+            if(args.length >= arity) {
+                return fn(...args)
+            }
+            else {
+                return nextCurried(args)
+            }
+        }
+    })([])
+}
+
+const looseCurryArrow = 
+    (fn, arity = fn.length, nextCurried) =>
+   // Define nextCurried function
+    (nextCurried = prevArgs => 
+        // Define curried function
+        (...nextArgs) => {
+            const args = [...prevArgs, ...nextArgs]
+            // We use >= because someone may supply extra args
+            if(args.length >= arity) {
+                return fn(...args)
+            }
+            else {
+                return nextCurried(args)
+            }
+        }
+    )([])
+
+
+function looseCurryUsage() {
+    const f1 = (a, b, c, d, e) => a + b + c + d + e 
+    const res = looseCurry(f1)(1, 2, 3)(4)(5)
+    console.log(res)
+    const res2 = looseCurryArrow(f1)(2, 3)(1)(4, 8, 0, 5)
+    console.log(res2)
+}
+looseCurryUsage()
